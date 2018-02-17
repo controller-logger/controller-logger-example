d7b17a09f1b660d0dd706b3155d2de5ae1422eba
Harshil Sharma <harshilsharma63@gmail.com>
2018-02-17 18:24:48 +0530

    updated for data scrubbing

--- src/main/java/com/harshil/controllerloggerconsumer/controller/FooBarController.java
+++ src/main/java/com/harshil/controllerloggerconsumer/controller/FooBarController.java
@@ -2,8 +2,10 @@ package com.harshil.controllerloggerconsumer.controller;
 
 import io.github.logger.controller.annotation.Logging;
 import io.github.logger.controller.annotation.NoLogging;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,16 @@ public class FooBarController {
 
     @RequestMapping(value = "/foo", method = RequestMethod.GET)
     @ResponseBody
-    public String foo() {
+    public String foo(
+            @RequestParam String accessToken,
+            @RequestParam String username,
+            @RequestParam String accountNumber,
+            @RequestParam String accountBalance,
+            @RequestParam String securityProtocol) {
+        // accessToken won't be logged being in default list of blacklisted params
+        // username will be logged
+        // accountNumber and accountBalance won't be logged being blacklisted by custom regex
+        // securityProtocol won't be logged being in custom list of blacklisted params
         return "This API will be logged.";
     }
 
